f57bc463ef3193d37c843d1516852d25db5ef8ba
Yucheol <ldsycson@byui.edu>
2025-02-09 01:14:50 -0700

    update index

---
+++ creditCard/card.js
@@ -0,0 +1,36 @@
+function isCardNumberValid(number) {
+	// normally we would contact a credit card service...but we don't know how to do that yet. So to keep things simple we will only accept one number
+	return number === '1234123412341234'
+}
+function displayError(msg) {
+	// display error message
+	document.querySelector('.errorMsg').innerHTML = msg
+}
+function submitHandler(event) {
+	event.preventDefault()
+	let errorMsg = ''
+	console.log(this.cardNumber.value)
+	// clear any previous errors
+	displayError('')
+	// check credit card number
+	if (isNaN(this.cardNumber.value)) {
+		// it is not a valid number
+		errorMsg += 'Card number is not a valid number\n'
+	} else if (!isCardNumberValid(this.cardNumber.value)) {
+		// it is a number, but is it valid?
+		errorMsg += 'Card number is not a valid card number\n'
+	}
+  if (new Date() > new Date(parseInt("20"+this.expirationYY.value),this.expirationMM.value-1)){
+    errorMsg += 'The card is expired'
+  }
+  console.log(errorMsg)
+  console.log(new Date(parseInt("20"+this.expirationYY.value),this.expirationMM.value-1))
+	if (errorMsg !== '') {
+		// there was an error. stop the form and display the errors.
+		displayError(errorMsg)
+		return false
+	}
+	return true
+}
+
+document.querySelector('#theForm').addEventListener('submit', submitHandler)
