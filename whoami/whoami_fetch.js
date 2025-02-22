9ce1e6723a06c0b1dbc35df8bd7abf09e9e52538
Yucheol <ldsycson@byui.edu>
2025-02-21 22:34:36 -0700

    js bind complete

--- whoami/whoami_fetch.js
+++ whoami/whoami_fetch.js
@@ -1,19 +1,64 @@
-const GITHUB_READONLY_TOKEN = "gh" + "p_4boFYPrxAUM59JRt" + "AmLqnyAfIwqQtx2QiqN9";
+const GITHUB_READONLY_TOKEN =
+  "gh" + "p_4boFYPrxAUM59JRt" + "AmLqnyAfIwqQtx2QiqN9";
 const USERNAME = "Yucheol-Son-BYUI";
+var REPOS;
+var CONTRIBUTION;
+// async function fetchRepositories(username) {
+//   const url = `https://api.github.com/users/${username}/repos?per_page=100`;
 
-async function fetchRepositories(username) {
-  const url = `https://api.github.com/users/${username}/repos?per_page=100`;
+//   const response = await fetch(url, {
+//     headers: { Authorization: `token ${GITHUB_READONLY_TOKEN}` },
+//   });
 
-  const response = await fetch(url, {
-    headers: { Authorization: `token ${GITHUB_READONLY_TOKEN}` },
+//   if (!response.ok) {
+//     throw new Error(`Failed to fetch repositories: ${response.status}`);
+//   }
+
+//   return await response.json();
+// }
+
+async function fetchRepositories() {
+  const query = `
+    {
+      user(login: "${USERNAME}") {
+        avatarUrl
+        repositories(first: 100){
+          nodes{
+            name
+            url
+            description
+            createdAt
+            updatedAt
+            primaryLanguage{
+              name
+            }
+            stargazerCount
+          }
+        }
+      }
+      
+    }`;
+
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      Authorization: `bearer ${GITHUB_READONLY_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
   });
 
   if (!response.ok) {
-    throw new Error(`Failed to fetch repositories: ${response.status}`);
+    throw new Error(
+      `Failed to fetch contribution calendar: ${response.status}`
+    );
   }
 
-  return await response.json();
+  const data = await response.json();
+  return data.data;
 }
+// 함수 호출 (원하는 페이지 번호 입력)
+REPOS = fetchRepositories(USERNAME);
 
 async function fetchContributionCalendar(username) {
   const query = `
@@ -53,35 +98,54 @@ async function fetchContributionCalendar(username) {
   const data = await response.json();
   return data.data.user.contributionsCollection.contributionCalendar;
 }
-var REPOS;
-var CONTRIBUTION;
-function main() {
 
-    REPOS = fetchRepositories(USERNAME);
-    CONTRIBUTION = fetchContributionCalendar(USERNAME);
+function main() {
+  REPOS = fetchRepositories(USERNAME);
+  CONTRIBUTION = fetchContributionCalendar(USERNAME);
 }
 
 main();
 
 var temp;
-function renderContributionCalendar(data){
+function renderContributionCalendar(data) {
   const calendarTable = document.querySelector("#calendar-table tbody");
-  console.log(data)
-  temp = data; 
-  let trHTML = Array(7).fill("")
-  for(day=0; day < 7; day++){
-    for(week=0;week < data.weeks.length;week++){
-      if(week == data.weeks.length - 1 && day >= data.weeks[week].contributionDays.length) continue;
+  console.log(data);
+  temp = data;
+  let trHTML = Array(7).fill("");
+  for (day = 0; day < 7; day++) {
+    for (week = 0; week < data.weeks.length; week++) {
+      if (
+        week == data.weeks.length - 1 &&
+        day >= data.weeks[week].contributionDays.length
+      )
+        continue;
 
       const cur = data.weeks[week].contributionDays[day];
-      trHTML[day] += `<td class="contributionDay" style="background-color:${cur.color}" data-date="${cur.date}" data-contribution-count="${cur.contributionCount}"></td>`
+      trHTML[
+        day
+      ] += `<td class="contributionDay" style="background-color:${cur.color}" data-date="${cur.date}" data-contribution-count="${cur.contributionCount}"></td>`;
     }
   }
   let result = "";
-  trHTML.forEach(tr => result += `<tr>` + tr + `</tr>`)
-  calendarTable.insertAdjacentHTML("afterbegin", result)
-  
+  trHTML.forEach((tr) => (result += `<tr>` + tr + `</tr>`));
+  calendarTable.insertAdjacentHTML("afterbegin", result);
 }
 
-REPOS.then(data => console.log(data))
-CONTRIBUTION.then(renderContributionCalendar)
+function renderRepositories(data) {
+  const repoList = document.querySelector("#projectList");
+  const avatarUrl = data.user.avatarUrl;
+  const repositories = data.user.repositories.nodes;
+  console.log(repositories)
+  let result = "";
+  repositories.forEach(repo => result += repoTemplate(repo))
+  repoList.insertAdjacentHTML("afterbegin", result);
+
+  function repoTemplate(repo){
+    console.log("repoTemplate: ",repo)
+    return `
+    <li class="project"><figure class="projectLogo"><img src="${avatarUrl}" width="30px" height="30px"></figure><h3><a href="${repo.url}" target="blank">${repo.name}</a></h3><span class="language">${repo.primaryLanguage.name}</span><p>${repo.description == null ? "no description" : repo.description}</p></li>`
+  }
+}
+
+REPOS.then(renderRepositories);
+CONTRIBUTION.then(renderContributionCalendar);
